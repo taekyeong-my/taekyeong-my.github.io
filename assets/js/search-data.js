252f614725ece5a67758ce1c8f373c417d0f6d59
taekyeong-my <94846458+taekyeong-my@users.noreply.github.com>
2025-04-10 09:30:32 +0000

    Deploying to gh-pages from @ taekyeong-my/taekyeong-my.github.io@efd0a404bc14cf57741913fa7b23c497d30290b6 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-cv",
           title: "CV",
-          description: "",
+          description: "Download the latest version of my CV here.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
