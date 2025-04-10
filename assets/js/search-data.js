a91c119d7e0f6874d57a18193b7b953416b2436e
taekyeong-my <94846458+taekyeong-my@users.noreply.github.com>
2025-04-10 09:18:27 +0000

    Deploying to gh-pages from @ taekyeong-my/taekyeong-my.github.io@7d1fa1bfd64089b516aefadf541cbdca2e22ab67 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -10,8 +10,8 @@ ninja.data = [{
       window.location.href = "/";
     },
   },{id: "nav-cv",
-          title: "cv",
-          description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
+          title: "CV",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
